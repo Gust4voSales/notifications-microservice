1ab67ff86af504992a08846437c70c7e3d6d9730
Gust4voSales <manoel0gustavo@gmail.com>
2022-12-16 18:45:14 -0300

    feat: day 1 - basic setup(routes, libs, DB)

---
+++ src/app.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { CreateNotificationBody } from './create-notification-body';
+import { PrismaService } from './prisma.service';
+
+@Controller('notifications')
+export class AppController {
+  constructor(private readonly prisma: PrismaService) {}
+
+  @Get()
+  list() {
+    return this.prisma.notification.findMany();
+  }
+
+  @Post()
+  create(@Body() body: CreateNotificationBody) {
+    console.log(body);
+  }
+}
